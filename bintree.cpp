4925d83ceb56037de34fd5e1471c1621bd6eec07
yuri971 <yurinakamora97@gmail.com>
2016-04-20 11:40:09 -0300

    Update bintree.cpp

--- bintree.cpp
+++ bintree.cpp
@@ -29,7 +29,63 @@ template <typename T>
 void BinTree <T> ::inserir(T t)
 {
   Node* pnode = this->root;
+  While(1)
+  {
+    if(t == pnode->info)
+    {
+      cout << "Ja existe esse elemento na arvore" << endl;
+      break;
+    }
+    if(t < pnode->info)
+    {
+      if(pnode->left == NULL)
+      {
+        Node* novo = new Node(t);
+        pnode->left = novo;
+        break;
+      }
+      else
+      {
+        pnode = pnode->left;
+      }
+    }
+    else
+    {
+      if(pnode->right == NULL)
+      {
+        Node* novo = new Node(t);
+        pnode->right = novo;
+        break;
+      }
+      else
+      {
+        pnode = pnode->right;
+      }
+    }
+  }
+}
+
+template <typename T>
+bool BinTree<T>::remover(T t)
+{
+  if(this->root == NULL)
+    return false;
   
+  if(this->root->info == t)
+  {
+    if(this->root->left == NULL && this->root->right == NULL)
+    {
+      delete this->root;
+    }
+    else
+    {
+      
+    }
+  }
+}
+
+bool RemoverLeft(T t) 
+{
   
 }
 
