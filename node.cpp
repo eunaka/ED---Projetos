b2da8b34060d524a34fa79b2d2041e707486bc00
yuri971 <yurinakamora97@gmail.com>
2016-05-06 11:29:53 -0300

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,91 +1,54 @@
 #include <iostream>
 #include "node.h"
 
-
-/*
- * constructor
- */
-template <typename T>
-Node <T>::Node(T info)
+/* construtor de Node */
+Node::Node(int info)
 {
     this->left = NULL;
     this->right = NULL;
     this->info = info;
 }
 
-/*
- * Destructor
- */
-template <typename T>
-Node <T>::~Node()
+/* destrutor de node */
+Node::~Node()
 {
-  this->Node = NULL;
   this->left = NULL;
   this->right = NULL;
+  std::cout << this->getInfo() << " removido\n\n";
 }
 
-/*
- * Retorna node da esquerda
- */
-template <typename T>
-Node<T>* Node<T>::getLeftNode()
+Node* Node::getLeftNode()
 {
     return this->left;
 }
 
-/*
- * Retorna node da direita
- */
-template <typename T>
-Node<T>* Node <T>::getRightNode()
+Node* Node::getRightNode()
 {
   return this->right;
 }
 
-/*
- * Retorna a informacao T do node
- */
-template <typename T>
-Node<T>* Node<T>::getInfo()
+int Node::getInfo()
 {
   return this->info;
 }
 
-/*
- * Altera a informacao T do node.
- */
-template<typename T>
-void setInfo(T info)
-{
-   this->info = info);
-}
-
-/*
- * Altera a referencia de node da esquerda
- */
-template <typename T>
-void Node<T>::setLeftNode(Node<T>* new_left)
+void Node::setLeftNode(Node* new_left)
 {
   this->left = new_left;
 }
 
-/*
- * Altera a referencia de node da direita
- */
-template <typename T>
-void Node<T>::setRightNode(Node<T>* new_right)
+void Node::setRightNode(Node* new_right)
 {
   this->right = new_right;
 }
 
-/*
- * Verifica se o node eh folha
- */
-template <typename T>
-bool Node<T>::isLeaf()
+bool Node::isLeaf()
 {
-    if(this->getLeftNode() == NULL && this->getRightNode() == NULL)
+    if(!this->getLeftNode() && !this->getRightNode())
       return 1;
 
     else return 0;
 }
+
+
+
